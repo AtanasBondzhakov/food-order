c0f4f7ff28e70a3862bd6b3c4cae283baaca5b6a
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-05-07 22:23:23 +0300

    Create Modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export default function Modal({
+    children,
+    open
+}) {
+    const dialog = useRef();
+
+    useEffect(() => {
+        if (open) {
+            dialog.current.showModal();
+        }
+    }, [open]);
+
+    return (
+        createPortal(<dialog>{children}</dialog>,
+            document.getElementById('modal'))
+    );
+};
