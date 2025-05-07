77a2d1efcfad239ea59af261f5aadc54946b93ff
atanasbondzhakov <atanas.b@yahoo.com>
2025-05-07 14:02:39 +0300

    Add cart reducer with add item functionality

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,3 +1,5 @@
+import { useReducer } from "react";
+
 const { createContext } = require("react");
 
 const CartContext = createContext({
@@ -6,6 +8,31 @@ const CartContext = createContext({
     removeItem: (id) => { }
 });
 
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_ITEM') {
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+
+        const updatedItems = [...state.items];
+
+        if (existingCartItemIndex > -1) {
+            const existingItem = state.items[existingCartItemIndex]
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingItem.quantity += 1
+            };
+
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems.push({ ...action.item, quantity: 1 });
+        }
+
+        return { ...state, items: updatedItems };
+    }
+
+}
+
 export function CartContextProvider({ children }) {
+    useReducer(cartReducer, {});
+
     return <CartContext.Provider>{children}</CartContext.Provider>
 };
