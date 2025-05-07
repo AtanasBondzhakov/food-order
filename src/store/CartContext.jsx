31e26c64ad9954e1f0506b62204f8a3882c5575d
atanasbondzhakov <atanas.b@yahoo.com>
2025-05-07 14:01:51 +0300

    Create CartContext

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,11 @@
+const { createContext } = require("react");
+
+const CartContext = createContext({
+    items: [],
+    addItem: (item) => { },
+    removeItem: (id) => { }
+});
+
+export function CartContextProvider({ children }) {
+    return <CartContext.Provider>{children}</CartContext.Provider>
+};
